b654211f623c6d6988976ed4ab7de4b9a86cd651
Taniska Chatterjee <chatterjeetaniska53@gmail.com>
2026-05-14 15:56:56 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.55%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,25 +1,30 @@
+#class Solution:
+    #def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]
+        #rec = []
+        #for x in nums1:
+            #index = nums2.index(x)
+            #next_greater = -1
+            #for j in range(index + 1, len(nums2)):
+               # if nums2[j] > x:
+                  #  next_greater = nums2[j]
+                 ## rec.append(next_greater)
+
+       # return rec
+
+
+
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        rec = []
 
-        for x in nums1:
+        mapp = {}
+        stack = []
 
-          
-            index = nums2.index(x)
+        for x in nums2:
+            while stack and x > stack[-1]:
+                smaller_elemnt = stack.pop()
+                mapp[smaller_elemnt] = x
+            stack.append(x)
 
-            next_greater = -1
-
-          
-            for j in range(index + 1, len(nums2)):
-
-                if nums2[j] > x:
-                    next_greater = nums2[j]
-                    break
-
-            rec.append(next_greater)
-
-        return rec
-
-
+        return [mapp.get(x, -1)for x in nums1]
 
     
